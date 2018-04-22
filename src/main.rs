ac578d9ea5bdaa82c018be0f442bf72f37569221
Kenneth Hedman <github@vitberget.se>
2018-04-22 21:14:48 +0200

    Now replaces one char in one word as optional password. Rust string manipulation... a bit frustrating to me as a Rust newbie.

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,14 @@ extern crate rand;
 
 use rand::Rng;
 use std::collections::HashMap;
+use std::cmp;
 use std::env;
 use std::fs::File;
 use std::io::Read;
 
 static DW_EN: &'static str = include_str!("../resources/diceware-en.txt");
 static DW_SV: &'static str = include_str!("../resources/diceware-sv.txt");
+static REPLACE_CHARS: &'static str = "~!#$%^&*()-=+[]\\{}:;\"'<>?/0123456789";
 
 fn dice() -> u8 {
     return rand::thread_rng().gen_range(1, 7);
@@ -64,24 +66,53 @@ fn read_words(filename: String, rolls: Vec<String>) -> HashMap<String, String> {
 fn dice_it(word_count: u8, filename: String) {
     let rolls = dice_strings(word_count);
 
-    print!("Dice rolls:");
+    let words = read_words(filename, rolls.clone());
+    let rolls_words: Vec<&String> = rolls.iter().map(|r| words.get(r).unwrap()).collect();
+
+    let r1 = rand::thread_rng().gen_range(1, cmp::min(7, word_count + 1));
+    let r1_word = rolls_words[(r1 - 1) as usize];
+    let (r1_word_size, r1_word_size_upper) = r1_word.chars().size_hint();
+    let r2 = (rand::thread_rng().gen_range(1, cmp::min(7, r1_word_size_upper.unwrap() + 1))) as u8;
+    let r3 = dice();
+    let r4 = dice();
+
+    let pw2 = replace_char(rolls_words.clone(), r1, r2, r3, r4);
+
+    print!("Password rolls   :");
     for s in rolls.iter() {
         print!(" {}", s);
     }
     println!();
-
-    let words = read_words(filename, rolls.clone());
-
-    let rolls_words: Vec<&String> = rolls.iter().map(|r| words.get(r).unwrap()).collect();
+    println!("Replace rolls    : {}{}{}{}", r1, r2, r3, r4);
 
     let mut first = true;
-    for w in rolls_words {
+    for w in rolls_words.clone() {
         if first {
-            print!("Password  : {}", w);
+            print!("Password option 1: {}", w);
         } else {
             print!("_{}", w);
         }
         first = false;
+    }
+    println!();
+
+    first = true;
+    let mut i = 1;
+    for w in rolls_words {
+        if first {
+            print!("Password option 2: ");
+        } else {
+            print!("_");
+        }
+
+        if i == r1 {
+            print!("{}", pw2)
+        } else {
+            print!("{}", w)
+        }
+
+        first = false;
+        i = i + 1;
     }
     println!();
 }
@@ -91,4 +122,54 @@ fn main() {
     let f = env::args().nth(2).unwrap_or("en".to_string());
 
     dice_it(n, f);
+}
+
+
+fn get_replace_char(third: u8, fourth: u8) -> char {
+    let i = ((fourth - 1) * 6 + (third - 1)) as usize;
+    let c = REPLACE_CHARS.chars().nth(i).unwrap();
+    return c;
+}
+
+fn replace_char(rolls_words: Vec<&String>, r1: u8, r2: u8, r3: u8, r4: u8) -> String {
+    let rc = get_replace_char(r3, r4);
+    let w = rolls_words[(r1 - 1) as usize];
+
+    let mut w2 = String::new();
+    let mut i = 1;
+    for c in w.chars() {
+        if i == r2 {
+            w2.push(rc);
+        } else {
+            w2.push(c);
+        }
+        i = i + 1;
+    }
+
+    return w2;
+}
+
+#[cfg(test)]
+mod tests {
+    use get_replace_char;
+
+    #[test]
+    fn replace_char1() {
+        assert_eq!(get_replace_char(1, 1), '~');
+    }
+
+    #[test]
+    fn replace_char2() {
+        assert_eq!(get_replace_char(6, 3), '}');
+    }
+
+    #[test]
+    fn replace_char3() {
+        assert_eq!(get_replace_char(2, 5), '/');
+    }
+
+    #[test]
+    fn replace_char4() {
+        assert_eq!(get_replace_char(5, 6), '8');
+    }
 }
