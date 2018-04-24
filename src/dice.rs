fb613df1332ec9dd10c2a795de0e450440e7f286
Kenneth Hedman <github@vitberget.se>
2018-04-24 12:32:05 +0200

    Moved dice to new module/file

---
+++ src/dice.rs
@@ -0,0 +1,23 @@
+extern crate rand;
+
+use rand::Rng;
+
+pub fn dice() -> u8 {
+    return rand::thread_rng().gen_range(1, 7);
+}
+
+fn five_dice() -> String {
+    return format!("{}{}{}{}{}", dice(), dice(), dice(), dice(), dice());
+}
+
+pub fn dice_strings(n: u8) -> Vec<String> {
+    let mut v = Vec::new();
+
+    let mut i = 0;
+    while i < n {
+        v.push(five_dice());
+        i = i + 1;
+    }
+
+    return v;
+}
--- src/main.rs
+++ src/main.rs
@@ -7,29 +7,12 @@ use std::env;
 use std::fs::File;
 use std::io::Read;
 
+use dice::{dice, dice_strings};
+mod dice;
+
 static DW_EN: &'static str = include_str!("../resources/diceware-en.txt");
 static DW_SV: &'static str = include_str!("../resources/diceware-sv.txt");
 static REPLACE_CHARS: &'static str = "~!#$%^&*()-=+[]\\{}:;\"'<>?/0123456789";
-
-fn dice() -> u8 {
-    return rand::thread_rng().gen_range(1, 7);
-}
-
-fn five_dice() -> String {
-    return format!("{}{}{}{}{}", dice(), dice(), dice(), dice(), dice());
-}
-
-fn dice_strings(n: u8) -> Vec<String> {
-    let mut v = Vec::new();
-
-    let mut i = 0;
-    while i < n {
-        v.push(five_dice());
-        i = i + 1;
-    }
-
-    return v;
-}
 
 fn read_words(filename: String, rolls: Vec<String>) -> HashMap<String, String> {
     let mut word_map = HashMap::new();
