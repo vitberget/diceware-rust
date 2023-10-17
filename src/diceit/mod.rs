426939193f0d50e92efd1cfdc8eef31133e12fce
Kenneth Hedman <github@vitberget.se>
2023-10-17 13:37:55 +0200

    refactor

--- src/diceit/mod.rs
+++ src/diceit/mod.rs
@@ -14,7 +14,7 @@ use self::{
 static DICTIONARY_ENGLISH: &'static str = include_str!("../../resources/diceware-en.txt");
 static DICTIONARY_SWEDISH: &'static str = include_str!("../../resources/diceware-sv.txt");
 
-fn read_words(filename: &str, rolls: Vec<String>) -> HashMap<String, String> {
+fn read_words(filename: &str, rolls: &Vec<String>) -> HashMap<String, String> {
     let mut word_map = HashMap::new();
 
     rolls.iter().for_each(|s| {
@@ -47,32 +47,32 @@ fn read_words(filename: &str, rolls: Vec<String>) -> HashMap<String, String> {
 
 fn print_rolls(rolls: Vec<String>, r1: u8, r2: u8, r3: u8, r4: u8) {
     print!("Password rolls   :");
-    rolls.iter().for_each(|s| print!(" {}", s));
+    rolls.iter().for_each(|roll| print!(" {roll}"));
     println!();
-    println!("Replace rolls    : {}{}{}{}", r1, r2, r3, r4);
+    println!("Replace rolls    : {r1}{r2}{r3}{r4}");
 }
 
-fn print_option1(rolls_words: Vec<&String>, word_count: u8, verbose: bool, separator: &str) {
-    let mut first = true;
-    rolls_words.into_iter().for_each(|word| {
-        if first {
+fn print_without_replace(rolls_words: &Vec<&String>, word_count: u8, verbose: bool, separator: &str) {
+    rolls_words.into_iter().enumerate().for_each(|(index,word)| {
+        if index == 0 {
             if verbose {
-                print!("Password option 1: ");
+                print!("Password with char replace   : ");
             }
-            print!("{}", word);
-            first = false;
+            print!("{word}");
         } else {
-            print!("{}{}", separator, word);
+            print!("{separator}{word}");
         }
     });
+
     if verbose {
         print!(" ({:.1} bits of entropy)", (word_count as f32) * 12.9);
     }
+
     println!();
 }
 
-fn print_option2(
-    rolls_words: Vec<&String>,
+fn print_with_replace(
+    rolls_words: &Vec<&String>,
     word_count: u8,
     verbose: bool,
     separator: &str,
@@ -85,19 +85,19 @@ fn print_option2(
         .clone()
         .iter()
         .enumerate()
-        .for_each(|(i, word)| {
-            if i == 0 {
+        .for_each(|(index, word)| {
+            if index == 0 {
                 if verbose {
-                    print!("Password option 2: ")
+                    print!("Password without char replace: ");
                 }
             } else {
-                print!("{}", separator);
+                print!("{separator}");
             }
 
-            if i + 1 == r1 as usize {
+            if index + 1 == r1 as usize {
                 print!("{}", replace_char(word, r2, r3, r4));
             } else {
-                print!("{}", word)
+                print!("{word}");
             }
         });
     if verbose {
@@ -109,11 +109,11 @@ fn print_option2(
     println!();
 }
 
-fn print_17_warning(rolls_words: Vec<&String>) {
-    let c: usize = rolls_words.iter().map(|w| w.len()).sum();
+fn print_17_char_warning(rolls_words: &Vec<&String>) {
+    let char_count: usize = rolls_words.iter().map(|w| w.len()).sum();
 
-    if c < 17 {
-        println!("!!! Word characters {} less than 17 !!!", c);
+    if char_count < 17 {
+        println!("!!! Word characters {char_count} less than 17 !!!");
     }
 }
 
@@ -126,11 +126,11 @@ pub(crate) fn dice_it(
 ) {
     let rolls = dice_strings(word_count);
 
-    let words = read_words(filename, rolls.clone());
-    let rolls_words: Vec<&String> = rolls.iter().map(|r| words.get(r).unwrap()).collect();
+    let word_map = read_words(filename, &rolls);
+    let words: Vec<&String> = rolls.iter().map(|r| word_map.get(r).unwrap()).collect();
 
     let r1 = rand::thread_rng().gen_range(1..=cmp::min(6, word_count)) as u8;
-    let r1_word = rolls_words[(r1 - 1) as usize];
+    let r1_word = words[(r1 - 1) as usize];
     let (_, r1_word_size_upper) = r1_word.chars().size_hint();
     let r2 = rand::thread_rng().gen_range(1..=cmp::min(6, r1_word_size_upper.unwrap())) as u8;
     let r3 = dice();
@@ -140,11 +140,11 @@ pub(crate) fn dice_it(
         print_rolls(rolls, r1, r2, r3, r4);
     }
     if verbose || !replace {
-        print_option1(rolls_words.clone(), word_count, verbose, separator);
+        print_without_replace(&words, word_count, verbose, separator);
     }
     if verbose || replace {
-        print_option2(
-            rolls_words.clone(),
+        print_with_replace(
+            &words,
             word_count,
             verbose,
             separator,
@@ -154,5 +154,5 @@ pub(crate) fn dice_it(
             r4,
         );
     }
-    print_17_warning(rolls_words);
+    print_17_char_warning(&words);
 }
